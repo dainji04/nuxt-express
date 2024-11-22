a1cb99d29069ceaf85d4538506a215a41c46babf
dainji04 <ndai31072004@gmail.com>
2024-11-22 15:47:37 +0700

    create pages

---
+++ clients/layouts/custom.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <HeaderCustom />
+        <slot />
+        <Footer />
+    </div>
+</template>
---
+++ clients/pages/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <section class="hero is-success is-halfheight">
+            <div class="hero-body">
+                <div class="">
+                    <p class="title">About me</p>
+                    <p class="subtitle">Half height subtitle</p>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
